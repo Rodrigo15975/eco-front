0e988db23d3327175deb60424462157f25c5db62
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-05 21:38:18 -0500

    feat: create config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  site_name: 'Next.js',
+  title: 'Next.js',
+  description: 'Next.js',
+  locale: 'en',
+  whatssapNumber: '+51 932548759',
+  whatsappContact: '51931022090',
+}
